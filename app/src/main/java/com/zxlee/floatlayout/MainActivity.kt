9d0dec5605bb22efbb674794e57214ed64b8ff81
zhengxian.lzx@alibaba-inc.com <zhengxian.lzx@alibaba-inc.com>
2018-03-21 09:21:56 +0800

    add test activity

--- app/src/main/java/com/zxlee/floatlayout/MainActivity.kt
+++ app/src/main/java/com/zxlee/floatlayout/MainActivity.kt
@@ -2,11 +2,23 @@ package com.zxlee.floatlayout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.zxlee.layout.FloatLayout
 
 class MainActivity : AppCompatActivity() {
+    var mFloatLayout: FloatLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mFloatLayout = findViewById(R.id.float_layout)
+        mFloatLayout!!.addView(Button(this))
+        mFloatLayout!!.addView(Button(this))
+        mFloatLayout!!.addView(Button(this))
+        mFloatLayout!!.addView(Button(this))
+        mFloatLayout!!.addView(Button(this))
+        mFloatLayout!!.addView(Button(this))
+        mFloatLayout!!.addView(Button(this))
+        mFloatLayout!!.addView(Button(this))
     }
 }
